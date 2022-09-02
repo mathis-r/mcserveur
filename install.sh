a6b6c1b06f83b2804fce629eae549bb0d6c61b05
Mathis R <98423827+mathis-r@users.noreply.github.com>
2022-09-02 17:12:19 +0200

    added install & uninstall scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Ce script vient de github.com/mathis-r/mcserveur
+
+cp list.sh /usr/bin/list.sh
+cp start.bash /usr/bin/start.bash
+cp stop.sh /usr/bin/stop.sh
+cp uninstall.sh /usr/bin/uninstall.sh
+
+echo "Installation faite !"
+echo "Vous pouvez à présent créer un serveur, puis le lancer avec sudo start.bash => Suivez les instructions du README"
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Ce script vient de github.com/mathis-r/mcserveur
+
+rm /usr/bin/list.sh
+rm /usr/bin/start.bash
+rm /usr/bin/stop.sh
+rm /usr/bin/uninstall.sh
+
+echo "Désinstaller !"
+echo "Les serveurs sont toujours là, mais vous pouvez les supprimer manuellement si vous avez envie"
