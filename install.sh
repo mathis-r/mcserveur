09e631aa06fe5524a40ca6df3f533a3cb9588439
Mathis R <98423827+mathis-r@users.noreply.github.com>
2022-09-02 17:37:26 +0200

    Ajout des permissions aux fichiers copiés

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 # Ce script vient de github.com/mathis-r/mcserveur
 
 cp list.sh /usr/bin/list.sh
+chmod x /usr/bin/list.sh
 cp start.bash /usr/bin/start.bash
+chmod +x /usr/bin/start.bash
 cp stop.sh /usr/bin/stop.sh
+chmod +x /usr/bin/stop.sh
 cp uninstall.sh /usr/bin/uninstall.sh
+chmod +x /usr/bin/uninstall.sh
 
 echo "Installation faite !"
 echo "Vous pouvez à présent créer un serveur, puis le lancer avec sudo start.bash => Suivez les instructions du README"
